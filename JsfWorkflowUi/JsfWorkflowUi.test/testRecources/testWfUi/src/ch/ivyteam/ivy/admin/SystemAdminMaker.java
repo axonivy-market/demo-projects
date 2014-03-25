51f21a72d45ebfba15e54a909320728282345bc3
Despot Jevric <despot.jevric@ivyteam.ch>
2014-03-25 15:15:01 +0000

    modify the systemAdminMaker, because the old not work anymore reviewed by
    Former-commit-id: feb349e9fa5ad00b35879e7600cffa7d7752b7a2

--- JsfWorkflowUi/JsfWorkflowUi.test/testRecources/testWfUi/src/ch/ivyteam/ivy/admin/SystemAdminMaker.java
+++ JsfWorkflowUi/JsfWorkflowUi.test/testRecources/testWfUi/src/ch/ivyteam/ivy/admin/SystemAdminMaker.java
@@ -2,13 +2,12 @@ package ch.ivyteam.ivy.admin;
 
 import java.util.concurrent.Callable;
 
+import ch.ivyteam.ivy.application.ApplicationConfigurationManagerNavigationUtil;
+import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.security.IPermissionGroup;
 import ch.ivyteam.ivy.security.ISecurityDescriptor;
-import ch.ivyteam.ivy.security.ISecurityManager;
 import ch.ivyteam.ivy.security.IUser;
 import ch.ivyteam.ivy.security.SecurityManagerFactory;
-import ch.ivyteam.ivy.server.IServer;
-import ch.ivyteam.ivy.server.ServerFactory;
 
 
 public class SystemAdminMaker 
@@ -16,18 +15,14 @@ public class SystemAdminMaker
 	public static void grantSystemAdminRightsTo(final IUser user) throws Exception
     {
     	SecurityManagerFactory.getSecurityManager().executeAsSystem(new Callable<Void>(){
-    	
-				@Override
+				@Override			
 				public Void call() throws Exception {
-					IServer server = ServerFactory.getServer();
-			        ISecurityManager securityManager = server.getSecurityManager();
-			        ISecurityDescriptor systemSecurityDescriptor = securityManager.getSystemSecurityDescriptor();
-					IPermissionGroup rootPermissionGroup = securityManager.getSystemSecurityDescriptor().getSecurityDescriptorType().getRootPermissionGroup();    
-	                systemSecurityDescriptor.grantPermissions(rootPermissionGroup, user);
-	                systemSecurityDescriptor.grantPermission(ch.ivyteam.ivy.security.IPermission.TASK_PAGE_ARCHIVE_READ_ALL, user);
-	                systemSecurityDescriptor.grantPermission(ch.ivyteam.ivy.security.IPermission.ADMINISTRATE_WORKFLOW, user);
-	                systemSecurityDescriptor.grantPermission(ch.ivyteam.ivy.security.IPermission.CASE_READ_ALL, user);
-					return null;
+					IApplication application = ApplicationConfigurationManagerNavigationUtil.getApplication(user.getSecurityContext());
+					ISecurityDescriptor securityDescriptor = application.getSecurityDescriptor();
+					IPermissionGroup rootPermissionGroup = securityDescriptor.getSecurityDescriptorType().getRootPermissionGroup();
+	                securityDescriptor.grantPermissions(rootPermissionGroup, user);
+	                
+	                return null;
 				}
 		});
     }
