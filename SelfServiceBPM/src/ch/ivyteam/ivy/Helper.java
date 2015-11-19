119b547bd36492047ee421dca985d6cab8bff696
Bruno BÃ¼tler <bruno.buetler@axonivy.com>
2015-11-19 14:48:33 +0000

    Initial import. DevDy Version Uses Portal Context Reviewed by Tamas
    Former-commit-id: 9747af65026a53ab1dbf5d2cff410e8cc84b3964

--- SelfServiceBPM/src/ch/ivyteam/ivy/Helper.java
+++ SelfServiceBPM/src/ch/ivyteam/ivy/Helper.java
@@ -12,11 +12,14 @@ public class Helper {
 	{
 		Collections.sort(userList, new Comparator<IUser>(){ 
             public int compare(IUser u1, IUser u2) { 
-            	if(u1.getFullName() == null || u2.getFullName()==null)
+            	try
             	{
-            		return u1.getName().compareTo(u2.getName());
-            	}
             	return (u1.getFullName().compareTo(u2.getFullName())); 
+            	}
+            	catch (Exception ex)
+            	{
+            		            	return 0;
+            	}
             }
 		}); 
         return userList;
