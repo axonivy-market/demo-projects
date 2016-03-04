6487052597f4e93ceef2230fd8723b74b7ac568c
Denis Huelin <denis.huelin@axonivy.com>
2016-03-04 15:09:17 +0000

    Restyling JSFWorkflowUi - Added table to inspect fired signals reviewed by rew
    Former-commit-id: fa7759b6729d99362d0022c082ab512aa0c92ed6

--- JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/signal/SignalService.java
+++ JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/signal/SignalService.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.workflow.query.SignalEventQuery;
 import ch.ivyteam.ivy.workflow.query.StartSignalEventElementQuery;
 import ch.ivyteam.ivy.workflow.query.TaskBoundarySignalEventReceiverQuery;
 import ch.ivyteam.ivy.workflow.signal.IBpmSignalService;
+import ch.ivyteam.ivy.workflow.signal.ISignalEvent;
 import ch.ivyteam.ivy.workflow.signal.IStartSignalEventElement;
 import ch.ivyteam.ivy.workflow.signal.ITaskBoundarySignalEventReceiver;
 
@@ -28,5 +30,11 @@ public class SignalService
     StartSignalEventElementQuery query = getBpmSignalService().receivers().createStartSignalQuery();
     return new ArrayList<IStartSignalEventElement>(query.executor().getResults());
   }
-
+  
+  public List<ISignalEvent> getFiredSignal()
+  {
+	  SignalEventQuery query = getBpmSignalService().history().createSignalEventQuery();
+	  return new ArrayList<ISignalEvent>(query.executor().getResults());
+  }
+  
 }
