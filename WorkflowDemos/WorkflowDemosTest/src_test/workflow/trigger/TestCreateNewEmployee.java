7bbdd0b217945b802cc7ac93a6530c6c5c9032a2
Barbara Brugger <barbara.brugger@axonivy.com>
2016-03-14 14:09:50 +0000

    Added Maven project nature and Maven dependencies reviewed by rew
    Former-commit-id: 0f662963f52e3ed7372c466f7a6a27a09776396d

---
+++ WorkflowDemos/WorkflowDemosTest/src_test/workflow/trigger/TestCreateNewEmployee.java
@@ -0,0 +1,11 @@
+package workflow.trigger;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class TestCreateNewEmployee {
+	@Test
+	public void test(){
+		Assertions.assertThat("Bla").isEqualTo("Bla");
+	}
+}
