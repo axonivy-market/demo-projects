62976644a5f536de38dfa22c59580e71b98518cb
Denis Huelin <denis.huelin@axonivy.com>
2016-07-29 08:38:26 +0000

    HtmlDialogDemosTest - Tested category Component reviewed by rew
    Former-commit-id: 7615ce40c21051382d6ed1ba2d2ec86e9c4306c7

---
+++ HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestComponent.java
@@ -0,0 +1,98 @@
+package ch.ivyteam.htmldialog.demo;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class WebTestComponent extends BaseWebTest
+{
+  @Test
+  public void testComponentDemo() throws Exception
+  {
+    startProcess("145D1849FACF0EAA/ComponentDemo.ivp");
+
+    driver.findElement(By.id("demoForm:personComponent:firstname")).sendKeys("testFirstName");
+    driver.findElement(By.id("demoForm:personComponent:lastname")).sendKeys("testName" + Keys.ENTER);
+    await(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='demoForm:panel']/table"),
+            "test"));
+
+    fillAddressComponent("delivery");
+    fillAddressComponent("billing");
+  }
+
+  private void fillAddressComponent(String prefix)
+  {
+    driver.findElement(By.id("demoForm:personComponent:" + prefix + "AddressComponent:street")).sendKeys(
+            prefix + "Street");
+    driver.findElement(By.id("demoForm:personComponent:" + prefix + "AddressComponent:country")).sendKeys(
+            prefix + "Country" + Keys.ENTER);
+    await(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='demoForm:panel']/table"),
+            prefix));
+  }
+
+  @Test
+  public void testCallbackDemo() throws Exception
+  {
+    startProcess("145D1849FACF0EAA/ComponentEventListenerDemo.ivp");
+
+    retryingFindClick(By.xpath("//*[@id='componentForm:detailListComponent:personsTable_data']/tr[2]"));
+    await(ExpectedConditions.textToBePresentInElementLocated(
+            By.id("componentForm:selectionResult_container"), "Person selected"));
+    await(ExpectedConditions.textToBePresentInElementLocated(
+            By.id("componentForm:selectionResult_container"), "Peter"));
+
+    retryingFindClick(By.xpath("//*[@id='componentForm:detailListComponent:personsTable_data']/tr[4]"));
+    retryingFindClick(By.id("componentForm:detailListComponent:fireEventButton"));
+    await(ExpectedConditions.textToBePresentInElementLocated(
+            By.id("componentForm:selectionResult_container"), "Fire event!"));
+  }
+
+  @Test
+  public void testPartialUpdate() throws Exception
+  {
+    startProcess("145D1849FACF0EAA/PartialUpdateDemo.ivp");
+
+    increaseCounter("A", 2);
+    assertPanelValue(2, 0);
+    increaseCounter("B", 1);
+    assertPanelValue(2, 3);
+    driver.findElement(By.id("Form:increaseAllButton")).click();
+    assertPanelValue(4, 4);
+  }
+
+  private void increaseCounter(String panel, int increaseClicks)
+  {
+    for (int i = 1; i <= increaseClicks; i++)
+    {
+      retryingFindClick(By.id("Form:increaseP" + panel + "Button"));
+    }
+  }
+
+  private void assertPanelValue(Integer expectedValueA, Integer ExpectedValueB)
+  {
+    await(ExpectedConditions.textToBePresentInElementLocated(By.id("Form:counterPanel_1_content"),
+            expectedValueA.toString()));
+    await(ExpectedConditions.textToBePresentInElementLocated(By.id("Form:counterPanel_2_content"),
+            ExpectedValueB.toString()));
+  }
+
+  private void retryingFindClick(By locator) throws StaleElementReferenceException
+  {
+    int attempts = 0;
+    while (attempts < 5)
+    {
+      try
+      {
+        driver.findElement(locator).click();
+        break;
+      }
+      catch (StaleElementReferenceException e)
+      {
+      }
+      attempts++;
+    }
+    return;
+  }
+}
