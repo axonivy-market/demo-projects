0085f2d3972bbe5c2882f992697ac9b059320c2f
Peter StÃ¶ckli <peter.stoeckli@axonivy.com>
2016-08-12 12:45:49 +0000

    XIVY-1245 Provide BusinessData Respository as ivy.xyz variable - added birthdates (demodate, edit, create) reviewed by nobody
    Former-commit-id: f95046cd93496e0ec5c8243ee077c680b620a89b

--- WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/wfdemo/businessdata/DemoDataCreator.java
+++ WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/wfdemo/businessdata/DemoDataCreator.java
@@ -7,6 +7,7 @@ import workflow.businessdata.Dossier;
 import workflow.businessdata.Person;
 import ch.ivyteam.ivy.business.data.store.BusinessData;
 import ch.ivyteam.ivy.business.data.store.BusinessDataRepository;
+import ch.ivyteam.ivy.scripting.objects.Date;
 
 public class DemoDataCreator {
 	
@@ -15,26 +16,27 @@ public class DemoDataCreator {
 		List<BusinessData<Dossier>> all = repo.findAll(Dossier.class);
 		
 		if (all.isEmpty()) {
-			createDemoDossier("HEISENBERG", "Werner", "Heisenberg", "97070", "Würzburg", "Germany");
-			createDemoDossier("PAULI", "Wolfgang", "Pauli", "1010", "Wien", "Austria");
-			createDemoDossier("NOETHER", "Emmy", "Noether", "91052", "Erlangen", "Germany");
-			createDemoDossier("TURING", "Alan", "Turing", "10000", "London", "England");
-			createDemoDossier("RAMANUJAN", "Srinivasa", "Ramanujan", "30000", "Erode", "India");
-			createDemoDossier("LOVELACE", "Ada", "Lovelace", "20000", "London", "England");
-			createDemoDossier("POINCARE", "Henri", "Poincaré", "54000", "Nancy", "France");
-			createDemoDossier("EULER", "Leonhard", "Euler", "4000", "Basel", "Switzerland");
-			createDemoDossier("VOLTA", "Alessandro", "Volta", "22100", "Como", "Italy");
-			createDemoDossier("NEUMANN", "John", "von Neumann", "1011", "Budapest", "Hungary");
+			createDemoDossier("HEISENBERG", "Werner", "Heisenberg", new Date(1901, 12, 5), "97070", "Würzburg", "Germany");
+			createDemoDossier("PAULI", "Wolfgang", "Pauli", new Date(1900, 04, 25), "1010", "Wien", "Austria");
+			createDemoDossier("NOETHER", "Emmy", "Noether", new Date(1882, 03, 23), "91052", "Erlangen", "Germany");
+			createDemoDossier("TURING", "Alan", "Turing", new Date(1912, 06, 23), "10000", "London", "England");
+			createDemoDossier("RAMANUJAN", "Srinivasa", "Ramanujan", new Date(1887, 12, 22), "30000", "Erode", "India");
+			createDemoDossier("LOVELACE", "Ada", "Lovelace", new Date(1815, 12, 10), "20000", "London", "England");
+			createDemoDossier("POINCARE", "Henri", "Poincaré", new Date(1854, 04, 29), "54000", "Nancy", "France");
+			createDemoDossier("EULER", "Leonhard", "Euler", new Date(1707, 04, 15), "4000", "Basel", "Switzerland");
+			createDemoDossier("VOLTA", "Alessandro", "Volta", new Date(1745, 02, 18), "22100", "Como", "Italy");
+			createDemoDossier("NEUMANN", "John", "von Neumann", new Date(1903, 12, 28), "1011", "Budapest", "Hungary");
 		}
 	}
 	
-	private static void createDemoDossier(String dossierName, String firstName, String lastName, String zip, String city, String country) {
+	private static void createDemoDossier(String dossierName, String firstName, String lastName, Date birthdate, String zip, String city, String country) {
 		Dossier dossier = new Dossier();
 		dossier.setName(dossierName);
 		
 		Person person = new Person();
 		person.setFirstName(firstName);
 		person.setLastName(lastName);
+		person.setBirthDate(birthdate);
 		
 		Address address = new Address();
 		address.setZip(zip);
