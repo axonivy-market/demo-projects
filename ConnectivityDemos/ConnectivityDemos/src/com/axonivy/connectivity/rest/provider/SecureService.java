21b244fcdbb21ae75b1872f51437bc356531c0e7
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2016-10-11 09:56:15 +0000

    XIVY-1361 Support javax.security annotations to define Access restrictions on service-method level - demonstrate usage of javax.security annotations in ConnectivityDemos Reviewed by PES
    Former-commit-id: 51f379483a45180e3972bd41078dc03a89ea46ae

---
+++ ConnectivityDemos/ConnectivityDemos/src/com/axonivy/connectivity/rest/provider/SecureService.java
@@ -0,0 +1,95 @@
+package com.axonivy.connectivity.rest.provider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.security.DenyAll;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+
+/**
+ * Demonstrates a service which protects it's methods with <code>javax.security</code> annotations.
+ * 
+ * <p>By default an ivy REST service requires clients to be authenticated with username and password via HTTP-BASIC.
+ * This behaviour can be adjusted with the following annotations:</p>
+ * <ul>
+ * 	<li>{@link javax.annotation.security.PermitAll}: allows unauthenticated access to anonymous users</li>
+ *  <li>{@link javax.annotation.security.RolesAllowed}: users must be authenticated and own the defined roles</li>
+ *  <li>{@link javax.annotation.security.DenyAll}: nobody is allowed to invoke this service</li>
+ * </ul>
+ * 
+ * All these annotations can be set either on the service class or on a specific method. Service class annotations
+ * are considered for all methods. But an annotation on a method always override the service class annotation.
+ * 
+ * @since 6.4.0
+ */
+@Singleton
+@Path("{applicationName}/admin")
+public class SecureService {
+	
+	private List<String> entries = new ArrayList<>(Arrays.asList("Hello world"));
+
+	/**
+	 *  {@link PermitAll}: no authentication required to call this method. Anonymous access granted.
+	 */
+	@GET
+	@PermitAll
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response showEntries()
+	{
+		return Response.status(Status.OK)
+				.entity(entries)
+				.build();
+	}
+	
+	/**
+	 * No <code>javax.security</code> annotation present: Defaults to HTTP-BASIC auth required.
+	 */
+	@PUT
+	@Consumes(MediaType.TEXT_PLAIN)
+	public Response addEntry(String newEntry)
+	{
+		entries.add(newEntry);
+		return Response.status(Status.OK)
+				.entity("Added entry '"+newEntry+"'")
+				.build();
+	}
+	
+	/** 
+	 * {@link RolesAllowed}: only HTTP-BASIC authenticated users which own the role 'Manager' are allowed to call this method. 
+	 */
+	@POST @Path("/{entryId}")
+	@RolesAllowed("Manager")
+	public Response updateEntry(@PathParam("entryId") int id, String newEntry)
+	{
+		entries.set(id, newEntry);
+		return Response.status(Status.OK)
+				.entity("Update entry with id ("+id+") to '"+newEntry+"'")
+				.build();
+	}
+	
+	/**
+	 * {@link DenyAll}: Access to this method is blocked for all users 
+	 */
+	@DELETE @Path("/{entryId}")
+	@DenyAll
+	public void removeEntry(@PathParam("entryId") int id)
+	{
+		entries.remove(id);
+	}
+	
+}
---
+++ ConnectivityDemos/ConnectivityDemosTest/src_test/com/axonivy/connectivity/rest/IntegrationTestSecureService.java
@@ -0,0 +1,97 @@
+package com.axonivy.connectivity.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.junit.Test;
+
+import com.axonivy.connectivity.rest.provider.SecureService;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+
+/**
+ * Tests the REST interface of the {@link SecureService}.
+ */
+public class IntegrationTestSecureService 
+{
+	@Test
+	public void invokePermitAllAnonymous()
+	{
+		Response response = createClient()
+			.target(adminServiceUri).request().get();
+		assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
+	}
+	
+	@Test
+	public void invokeDefaultBasicAuthMethod()
+	{
+		Response noAuthResponse = createClient()
+			.target(adminServiceUri).request()
+			.put(Entity.entity("my new entry", MediaType.TEXT_PLAIN));
+		assertThat(noAuthResponse.getStatus()).isEqualTo(Status.UNAUTHORIZED.getStatusCode());
+		
+		Response response = createAuthenticatedClient()
+			.target(adminServiceUri).request()
+			.put(Entity.entity("my new entry", MediaType.TEXT_PLAIN));
+		assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
+	}
+	
+	@Test
+	public void invokeAdminRoleMethod()
+	{
+		Response noAuthResponse = createClient()
+			.target(adminServiceUri).path("/0").request()
+			.post(Entity.entity("my new entry", MediaType.TEXT_PLAIN));
+		assertThat(noAuthResponse.getStatus()).isEqualTo(Status.UNAUTHORIZED.getStatusCode());
+		
+		Response response = createAuthenticatedClient()
+			.target(adminServiceUri).path("/0").request()
+			.post(Entity.entity("my new entry", MediaType.TEXT_PLAIN));
+		assertThat(response.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+
+		Response wrongRoleResponse = createClient()
+			.register(HttpAuthenticationFeature.basic("theBoss", "theBoss"))
+			.target(adminServiceUri).path("/0").request()
+			.post(Entity.entity("my new entry", MediaType.TEXT_PLAIN));
+		assertThat(wrongRoleResponse.getStatus()).isEqualTo(Status.OK.getStatusCode());
+		
+	}
+	
+	@Test
+	public void invokeDenyAllMethod()
+	{
+		Response response = createAuthenticatedClient()
+			.target(adminServiceUri+"/0").request()
+			.delete();
+		assertThat(response.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+	}
+	
+	public IntegrationTestSecureService() {
+		this.adminServiceUri = EngineUrl.rest()+"/admin";
+	}
+
+	public static final String REST_USER = "restUser";
+	private final String adminServiceUri;
+	
+	private static Client createAuthenticatedClient() {
+		Client httpClient = createClient();
+	    httpClient.register(HttpAuthenticationFeature.basic(REST_USER, REST_USER));
+		return httpClient;
+	}
+	
+	private static Client createClient() {
+		Client httpClient = ClientBuilder.newClient();
+	    httpClient.register(JacksonJsonProvider.class);
+	    httpClient.register(new LoggingFilter());
+		return httpClient;
+	}
+
+}
