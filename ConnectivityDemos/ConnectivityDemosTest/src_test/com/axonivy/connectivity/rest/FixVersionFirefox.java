09a39c3c5dac70a5cac1fd99d431c811daeee3da
Denis Huelin <denis.huelin@axonivy.com>
2016-10-20 09:46:58 +0000

    Fixed ConnectivityDemosTest - Using FixFirefox now reviewed by rew
    Former-commit-id: ef1c4443f465c7638495737aa265651bb1ab79ac

---
+++ ConnectivityDemos/ConnectivityDemosTest/src_test/com/axonivy/connectivity/rest/FixVersionFirefox.java
@@ -0,0 +1,65 @@
+package com.axonivy.connectivity.rest;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+/**
+ * Helpers to use a fixed (portable) version of firefox. So selenium doesn't
+ * need an update to be compatible with the current firefox version under test.
+ * 
+ * <p>
+ * The portable firefox must be installed as follows:
+ * 
+ * <pre>
+ * - install with a FF portable installer binary
+ * - disconnect your network adapter
+ * - start the portable firefox
+ * - deactivate update via about:config
+ *   * app.update.enabled = false
+ *   * app.update.url = ""
+ *   * app.update.url.manual = ""
+ * - close firefox
+ * - reconnect your network adapter
+ * - start firefox several times -> no updates should be installed
+ * </pre>
+ */
+public class FixVersionFirefox
+{
+  private static final String FIREFOX_PORTABLE_PATH_PROPERTY = "firefox.portable.path";
+  private static File FIREFOX_PORTABLE_DIR;
+  static
+  {
+    String firefoxPortablePath = System.getProperty(FIREFOX_PORTABLE_PATH_PROPERTY);
+    if (firefoxPortablePath != null)
+    {
+      FIREFOX_PORTABLE_DIR = new File(firefoxPortablePath);
+    }
+  }
+
+  public static FirefoxProfile loadFirefoxProfile()
+  {
+    if (FIREFOX_PORTABLE_DIR != null)
+    {
+      File profileDir = new File(FIREFOX_PORTABLE_DIR, "Data/profile");
+      System.out.println("Using Firefox portable profile " + profileDir);
+      return new FirefoxProfile(profileDir);
+    }
+    return new FirefoxProfile();
+  }
+
+  public static WebDriver createWebDriver(FirefoxProfile profile)
+  {
+    if (FIREFOX_PORTABLE_DIR != null)
+    {
+      File firefoxBinaryFile = new File(FIREFOX_PORTABLE_DIR, "App/Firefox/firefox.exe");
+      System.out.println("Using Firefox portable binary " + firefoxBinaryFile);
+      FirefoxBinary ffBinary = new FirefoxBinary(firefoxBinaryFile);
+      return new FirefoxDriver(ffBinary, profile);
+    }
+    return new FirefoxDriver(profile);
+  }
+}
--- ConnectivityDemos/ConnectivityDemosTest/src_test/com/axonivy/connectivity/rest/WebTestTwitterRestClient.java
+++ ConnectivityDemos/ConnectivityDemosTest/src_test/com/axonivy/connectivity/rest/WebTestTwitterRestClient.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,70 +12,81 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class WebTestTwitterRestClient {
+public class WebTestTwitterRestClient
+{
 
-	private WebDriver driver;
+  private WebDriver driver;
 
-	@Test
-	public void readTwitterTweetsViaRest()
-	{
-		String startUrl = EngineUrl.process()+"/ConnectivityDemos/1547634C396BBB3A/userTimeline.ivp";
-		driver.get(startUrl);
-		
-		if (driver.getTitle().equals("TwitterAuthorization"))
-		{
-			authenticate();
-		}
-		
-		new WebDriverWait(driver, 12).until(ExpectedConditions.titleIs("TwitterUserTimeline"));
-		WebElement tweets = driver.findElement(By.id("form:tweetTable"));
-		assertThat(tweets.findElements(By.tagName("tr")).size())
-			.as("tweets should be displayed")
-			.isGreaterThan(10);
-	}
+  @Test
+  public void readTwitterTweetsViaRest() 
+  {
+    String startUrl = EngineUrl.process() + "/ConnectivityDemos/1547634C396BBB3A/userTimeline.ivp";
+    driver.get(startUrl);
 
-	private void authenticate() 
-	{
-		String ivyAuthWindowHandle = driver.getWindowHandle();
-		driver.findElement(By.id("authLink")).click();
-		List<String> handles = new ArrayList<>(driver.getWindowHandles());
-		assertThat(handles.size()).isEqualTo(2);
-		handles.remove(ivyAuthWindowHandle);
-		String twitterWindowHandle = handles.get(0);
-		driver.switchTo().window(twitterWindowHandle);
-		
-		new WebDriverWait(driver, 10)
-			.until(ExpectedConditions.elementToBeClickable(By.id("username_or_email")))
-			.sendKeys("ivyTeamTester");
-		driver.findElement(By.id("password")).sendKeys("r2l6AmivZ0q9JgXYg7Fp");
-		driver.findElement(By.id("allow")).click();
-		
-		WebElement pinDiv = driver.findElement(By.id("oauth_pin"));
-		String verification = pinDiv.findElement(By.tagName("code")).getText();
-		assertThat(verification).isNotEmpty();
-		driver.close(); // close twitter page
-		
-		driver.switchTo().window(ivyAuthWindowHandle);
-		driver.findElement(By.id("form:verificationId")).sendKeys(verification);
-		new WebDriverWait(driver, 10)
-			.until(ExpectedConditions.elementToBeClickable(By.id("form:proceed")))
-			.click();
-	}
-	
-	@Before
-	public void setUp()
-	{
-		driver = new FirefoxDriver();
-	}
-	
-	@After
-	public void tearDown()
-	{
-		driver.close();
-	}
-	
+    if (driver.getTitle().equals("TwitterAuthorization"))
+    {
+      authenticate();
+    }
+
+    new WebDriverWait(driver, 12).until(ExpectedConditions.titleIs("TwitterUserTimeline"));
+    WebElement tweets = driver.findElement(By.id("form:tweetTable"));
+    assertThat(tweets.findElements(By.tagName("tr")).size())
+            .as("tweets should be displayed")
+            .isGreaterThan(10);
+  }
+
+  private void authenticate() 
+  {
+    String ivyAuthWindowHandle = driver.getWindowHandle();
+    driver.findElement(By.id("authLink")).click();
+    List<String> handles = new ArrayList<>(driver.getWindowHandles());
+    assertThat(handles.size()).isEqualTo(2);
+    handles.remove(ivyAuthWindowHandle);
+    String twitterWindowHandle = handles.get(0);
+    driver.switchTo().window(twitterWindowHandle);
+
+    new WebDriverWait(driver, 10)
+            .until(ExpectedConditions.elementToBeClickable(By.id("username_or_email")))
+            .sendKeys("ivyTeamTester");
+    new WebDriverWait(driver, 10)
+            .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@type='password']")));
+    driver.findElement(By.xpath("//*[@type='password']")).sendKeys("r2l6AmivZ0q9JgXYg7Fp");
+    
+    driver.findElement(By.xpath("//*[@type='password']")).submit();
+    WebElement pinDiv = driver.findElement(By.id("oauth_pin"));
+    String verification = pinDiv.findElement(By.tagName("code")).getText();
+    assertThat(verification).isNotEmpty();
+    driver.close(); // close twitter page
+
+    driver.switchTo().window(ivyAuthWindowHandle);
+    driver.findElement(By.id("form:verificationId")).sendKeys(verification);
+    new WebDriverWait(driver, 10)
+            .until(ExpectedConditions.elementToBeClickable(By.id("form:proceed")))
+            .click();
+  }
+
+  @Before
+  public void setUp()
+  {
+    driver = createDriver();
+  }
+
+  @After
+  public void tearDown()
+  {
+    driver.close();
+  }
+
+  private final WebDriver createDriver()
+  {
+    FirefoxProfile profile = FixVersionFirefox.loadFirefoxProfile();
+    WebDriver localDriver = FixVersionFirefox.createWebDriver(profile);
+
+    localDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    return localDriver;
+  }
 }
