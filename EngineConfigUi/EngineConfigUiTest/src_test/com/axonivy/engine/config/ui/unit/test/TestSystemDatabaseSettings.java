ef104f96085665bea9521a4e8cb163051c2a2d4d
Denis Huelin <denis.huelin@axonivy.com>
2017-02-14 15:30:30 +0000

    Story XIVY-1378: System DB Creation - Delete DBs after test Reviewed by dhu
    Former-commit-id: 6dc401be9d7325f5e343e16c2b7c801f48d4e387

--- EngineConfigUi/EngineConfigUiTest/src_test/com/axonivy/engine/config/ui/unit/test/TestSystemDatabaseSettings.java
+++ EngineConfigUi/EngineConfigUiTest/src_test/com/axonivy/engine/config/ui/unit/test/TestSystemDatabaseSettings.java
@@ -2,6 +2,7 @@ package com.axonivy.engine.config.ui.unit.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 import mockit.NonStrictExpectations;
@@ -10,7 +11,9 @@ import org.apache.commons.lang.math.RandomUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import ch.ivyteam.db.jdbc.DatabaseConnectionConfiguration;
 import ch.ivyteam.db.jdbc.DatabaseProduct;
+import ch.ivyteam.db.jdbc.DatabaseUtil;
 import ch.ivyteam.db.jdbc.JdbcDriver;
 import ch.ivyteam.ivy.server.configuration.system.db.ConnectionState;
 import ch.ivyteam.licence.SignedLicence;
@@ -46,22 +49,36 @@ public class TestSystemDatabaseSettings
     assertThat(SystemDatabaseConnecting.testConnection(configData)).isEqualTo(
             ConnectionState.CONNECTION_FAILED);
 
-    Properties properties = new Properties();
-    properties.put("databaseName", DBName);
-    configData.setCreationParameters(properties);
-    SystemDatabaseConnecting.createDatabase(configData);
+    createDatabase(configData);
+    
     assertThat(SystemDatabaseConnecting.testConnection(configData)).isEqualTo(ConnectionState.CONNECTED);
-
+    
+    dropDatabase(configData);
   }
+
 
   @Test
   public void testCreating() throws Exception
   {
     ConfigData configData = getLocalMySqlSettings();
+    
+    createDatabase(configData);
+    
+    dropDatabase(configData);
+  }
+
+  private void createDatabase(ConfigData configData) throws Exception
+  {
     Properties properties = new Properties();
-    properties.put("databaseName", DBName + "_newCreation");
+    properties.put("databaseName", DBName);
     configData.setCreationParameters(properties);
     SystemDatabaseConnecting.createDatabase(configData);
+  }
+
+  private void dropDatabase(ConfigData configData) throws SQLException
+  {
+    DatabaseConnectionConfiguration dbConnectionConfig = SystemDatabaseSettings.createConfiguration(configData);
+    DatabaseUtil.dropDatabase(DBName, dbConnectionConfig);
   }
 
   @Test
@@ -78,7 +95,7 @@ public class TestSystemDatabaseSettings
     configData.setUsername("admin");
     configData.setPassword("nimda");
     configData.setPort("3306");
-    DBName= "tmp_engineConfigUi_testing_"+ ((Integer)RandomUtils.nextInt()).toString();
+    DBName = "tmp_engineConfigUi_testing_" + ((Integer) RandomUtils.nextInt()).toString();
     configData.setDatabaseName(DBName);
     JdbcDriver[] jdbcDriversForDriverName = JdbcDriver.getJdbcDriversForDriverName("com.mysql.jdbc.Driver");
     configData.setDriver(jdbcDriversForDriverName[0]);
