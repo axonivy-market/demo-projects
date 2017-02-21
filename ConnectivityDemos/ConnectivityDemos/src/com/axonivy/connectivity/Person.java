1a103701fc4fe7e9ce2988d4d9423314efe570b9
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2017-02-21 12:59:14 +0000

    XIVY-1688 Enhance RestClientCall Inscription Mask - similar hashCode, equals and toString implementation as in CompositeObject. Reviewed by RWEI
    Former-commit-id: 67c9e678a40b76b6d9e7f99dc0a36c2cf2af7a09

--- ConnectivityDemos/ConnectivityDemos/src/com/axonivy/connectivity/Person.java
+++ ConnectivityDemos/ConnectivityDemos/src/com/axonivy/connectivity/Person.java
@@ -3,6 +3,10 @@ package com.axonivy.connectivity;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 @XmlRootElement(name = "person")
 public class Person
@@ -21,12 +25,13 @@ public class Person
   {
     id = _id;
   }
-  
+
   @XmlElement
   public java.lang.String getFirstname()
   {
     return firstname;
   }
+
   public void setFirstname(java.lang.String _firstname)
   {
     firstname = _firstname;
@@ -41,6 +46,45 @@ public class Person
   public void setLastname(java.lang.String _lastname)
   {
     lastname = _lastname;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (obj == null)
+    {
+      return false;
+    }
+    if (this == obj)
+    {
+      return true;
+    }
+    if (this.getClass() != obj.getClass())
+    {
+      return false;
+    }
+    Person other = (Person) obj;
+    return new EqualsBuilder()
+            .append(id, other.id)
+            .append(firstname, other.firstname)
+            .append(lastname, other.lastname)
+            .isEquals();
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return new HashCodeBuilder()
+      .append(id)
+      .append(firstname)
+      .append(lastname)
+      .toHashCode();
+  }
+
+  @Override
+  public String toString()
+  {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
   }
 
 }
