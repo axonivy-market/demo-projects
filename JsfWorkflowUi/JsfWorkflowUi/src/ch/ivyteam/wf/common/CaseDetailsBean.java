bb85b09f73d84396231f5b5b6426abbf1065e956
Peter StÃ¶ckli <peter.stoeckli@axonivy.com>
2017-07-05 14:59:49 +0000

    XIVY-1685: Enhance Workflow UI to show startable Case Maps and the Case Map state of a business case - try use new API for JSF wf UI reviewed by nobody Former-commit-id: e6717e2c02a31a51e7de27656971146b297d670e

--- JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/common/CaseDetailsBean.java
+++ JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/common/CaseDetailsBean.java
@@ -6,27 +6,28 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import ch.ivyteam.ivy.casemap.runtime.CaseMapService;
+import ch.ivyteam.ivy.casemap.runtime.ICaseMapService;
 import ch.ivyteam.ivy.casemap.runtime.model.IStage;
 import ch.ivyteam.ivy.workflow.ICase;
 
 @ManagedBean
 @ViewScoped
 public class CaseDetailsBean {
-	
+
 	public List<IStage> getStages(ICase wfCase) {
 		if (wfCase == null) {
 			return Collections.emptyList();
 		}
-		return CaseMapService.get().stages().getAll(wfCase.getBusinessCase());
+		return ICaseMapService.get(wfCase.getBusinessCase()).findCaseMap().getStages();
 	}
 
 	public int getIndexOfCurrentStage(ICase wfCase) {
 		if (wfCase == null) {
 			return -1;
 		}
+
 		return getStages(wfCase).indexOf(
-				CaseMapService.get().stages()
-						.findCurrent(wfCase.getBusinessCase()));
+				ICaseMapService.get(wfCase.getBusinessCase())
+						.findCurrentStage());
 	}
 }
