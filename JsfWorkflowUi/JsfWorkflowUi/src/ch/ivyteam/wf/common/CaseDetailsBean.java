870d521ddf9bdce7b14b265c0730c7d824d60345
Flavio Sadeghi <flavio.sadeghi@axonivy.com>
2017-08-08 07:37:52 +0000

    Adjust JsfWorkflowUi stage component - Only show stages which are not in the flow when such a stage is active reviewed by pes
    Former-commit-id: 8b5ffbcb5dc46e8f9c2003b6213acd70c2411857

--- JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/common/CaseDetailsBean.java
+++ JsfWorkflowUi/JsfWorkflowUi/src/ch/ivyteam/wf/common/CaseDetailsBean.java
@@ -16,26 +16,54 @@ import ch.ivyteam.ivy.workflow.ICase;
 @ViewScoped
 public class CaseDetailsBean {
 
-	public List<IStage> getStages(ICase wfCase) {
-		if (wfCase == null) {
-			return Collections.emptyList();
-		}
-		ICaseMap caseMap = getCaseMapService(wfCase.getBusinessCase()).findCaseMap();
-		if (caseMap == null) {
-			return Collections.emptyList();
-		}
-		
-		return caseMap.getStages();
-	}
+  private List<IStage> getStages(ICase wfCase) {
+    if (wfCase == null) {
+      return Collections.emptyList();
+    }
+    ICaseMap caseMap = getCaseMapService(wfCase.getBusinessCase()).findCaseMap();
+    if (caseMap == null) {
+      return Collections.emptyList();
+    }
 
-	public int getIndexOfCurrentStage(ICase wfCase) {
-		if (wfCase == null) {
-			return -1;
-		}
-		return getStages(wfCase).indexOf(getCaseMapService(wfCase).findCurrentStage());
-	}
+    return caseMap.getStages();
+  }
 
-	private ICaseMapService getCaseMapService(ICase wfCase) {
-		return Ivy.get(ICaseMapService.class).getCaseMapService(wfCase.getBusinessCase());
-	}
+  public int getIndexOfCurrentStage(ICase wfCase) {
+    if (wfCase == null) {
+      return -1;
+    }
+    return getStages(wfCase).indexOf(getCaseMapService(wfCase).findCurrentStage());
+  }
+
+  private ICaseMapService getCaseMapService(ICase wfCase) {
+    return Ivy.get(ICaseMapService.class).getCaseMapService(wfCase.getBusinessCase());
+  }
+
+  public List<IStage> getStagesBaseOnCurrentStage(ICase wfCase) {
+    if (wfCase == null) {
+      return Collections.emptyList();
+    }
+    ICaseMap caseMap = getCaseMapService(wfCase.getBusinessCase()).findCaseMap();
+    if (caseMap == null) {
+      return Collections.emptyList();
+    }
+    List<IStage> stages = caseMap.getStages();
+    // if current stage is in the main flow, do not display secondary flow
+    int firstTerminatingStageIndex = getFirstTerminatingStageIndex(stages);
+    if (firstTerminatingStageIndex >= getIndexOfCurrentStage(wfCase)) {
+      stages = stages.subList(0, firstTerminatingStageIndex + 1);
+    }
+    return stages;
+  }
+
+  private int getFirstTerminatingStageIndex(List<IStage> stages) {
+    int counter = 0;
+    for (IStage stage : stages) {
+      if (stage.isTerminating()) {
+        return counter;
+      }
+      counter++;
+    }
+    return -1;
+  }
 }
