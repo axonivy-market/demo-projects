4b39437571bed1541cbfd10f48084eea9acc5dcf
Andreas Rusch <andreas.rusch@axonivy.com>
2018-05-14 09:08:43 +0200

    Added FixVersionFirefox.java to add headless run and support for firefox.portable.path property.

---
+++ EngineConfigUi/EngineConfigUiTest/src_test/com/axonivy/engine/config/ui/web/test/geckodriver/FixVersionFirefox.java
@@ -0,0 +1,48 @@
+package com.axonivy.engine.config.ui.web.test.geckodriver;
+
+import java.io.File;
+import java.util.logging.Level;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+/**
+ * Helper class to use a fixed (portable) version of Firefox.
+ */
+public class FixVersionFirefox
+{
+  private static final String FIREFOX_PORTABLE_PATH_PROPERTY = "firefox.portable.path";
+  private static File FIREFOX_PORTABLE_DIR = readFirefoxPath();
+
+  public static WebDriver createWebDriver(FirefoxProfile profile)
+  {
+    if (FIREFOX_PORTABLE_DIR != null)
+    {
+      File firefoxBinaryFile = new File(FIREFOX_PORTABLE_DIR, "App/Firefox/firefox.exe");
+      System.out.println("Using Firefox portable binary " + firefoxBinaryFile);
+      FirefoxBinary ffBinary = new FirefoxBinary(firefoxBinaryFile);
+      ffBinary.addCommandLineOptions("--headless");
+      FirefoxOptions options = new FirefoxOptions().setBinary(ffBinary).setProfile(profile);
+      return new FirefoxDriver(options);
+    }
+    FirefoxBinary ffBinary = new FirefoxBinary();
+    ffBinary.addCommandLineOptions("--headless");
+    FirefoxOptions options = new FirefoxOptions().setLogLevel(Level.SEVERE).setBinary(ffBinary)
+            .setProfile(profile);
+    return new FirefoxDriver(options);
+  }
+
+  private static File readFirefoxPath()
+  {
+    String firefoxPortablePath = System
+            .getProperty(FIREFOX_PORTABLE_PATH_PROPERTY);
+    if (firefoxPortablePath != null)
+    {
+      return new File(firefoxPortablePath);
+    }
+    return null;
+  }
+}
