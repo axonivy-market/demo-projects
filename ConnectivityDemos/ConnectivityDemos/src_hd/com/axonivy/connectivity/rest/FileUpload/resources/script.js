eca42453f76155141c6fee6dd782001cb63e2367
Jacek Lajdecki <jacek.lajdecki@axonivy.com>
2019-05-21 13:04:20 +0200

    XIVY-3097 move JavaScript code to its own file

---
+++ ConnectivityDemos/ConnectivityDemos/src_hd/com/axonivy/connectivity/rest/FileUpload/resources/script.js
@@ -0,0 +1,55 @@
+function uploadFileJS(file) {
+	var form = new FormData();
+	var filename = file[0].name;
+
+	form.append("file", file[0]);
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://localhost:8081/ivy/api/designer/fileUpload",
+		"method": "PUT",
+		"headers": { "X-Requested-By": "ivy" },
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form
+	};
+
+	addToTable(filename);
+
+	jsf.ajax.request(this, event, {
+		render: 'form:dataTable ',
+		'CLIENT_BEHAVIOR_RENDERING_MODE': 'UNOBSTRUSIVE'
+	});
+
+
+	return $.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
+
+var logic = {
+	addJsToList: function (data, callback) {
+		$.ajax({
+			url: "rest/method/addJsToList",
+			type: "POST",
+			dataType: 'json',
+			data: JSON.stringify(data),
+			success: function (data) {
+				if (data.ivyRedirectUrl) {
+					window.location.replace(data.ivyRedirectUrl);
+				} else {
+					return callback(data);
+				}
+			}
+		}).error(function () { alert("Server error when calling method addJsToList"); });
+	},
+	loaded: true
+};
+
+function addToTable(fileName) {
+	logic.addJsToList(
+		{ "fileName": fileName },
+		function (returnData) { });
+};
