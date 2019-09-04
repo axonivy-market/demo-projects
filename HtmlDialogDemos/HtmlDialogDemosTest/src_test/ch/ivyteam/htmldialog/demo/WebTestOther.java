bfc62f99309d68b2800be1abbbd1b7cfbc39398e
Andreas Rusch <andreas.rusch@axonivy.com>
2019-09-04 14:13:04 +0200

    XIVY-3276 Added test for client side validation sample

--- HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestOther.java
+++ HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestOther.java
@@ -57,6 +57,23 @@ public class WebTestOther extends BaseWebTest
     openAndValidate(managedBeanPoperty);
   }
 
+  @Test
+  public void testClientSideValidation()
+  {
+    startProcess("150425B095B4FB54/ClientSideValidationDemo.ivp");
+    driver.findElement(By.id("form:firstName")).sendKeys("Someone");
+    driver.findElement(By.id("form:personName")).sendKeys("Else");
+    driver.findElement(By.id("form:personDateOfBirth")).sendKeys("1990-01-01");
+    driver.findElement(By.id("form:addressZip")).sendKeys("8000");
+    driver.findElement(By.id("form:addressCity")).sendKeys("Zürich");
+    driver.findElement(By.id("form:addressCountry")).sendKeys("CH");
+    driver.findElement(By.id("form:email")).sendKeys("someone.else@admin.ch");
+    driver.findElement(By.id("form:timeOfArrival")).sendKeys("13:15");
+    driver.findElement(By.id("form:proceed")).click();
+    await(textToBePresentInElementLocated(By.id("welcomeText"),
+            "Welcome to Axon.ivy Html Dialog Demos"));
+  }
+
   private void openAndValidate(String managedBeanPoperty)
   {
     startProcess("145D1862CF17F2C9/ManagedBeanDemo.ivp");
