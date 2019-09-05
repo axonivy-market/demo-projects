ae41f199ab828fd9208cce2b64033b35508a2597
Andreas Rusch <andreas.rusch@axonivy.com>
2019-09-05 14:57:26 +0200

    XIVY-3276 Added test if client side validation works

--- HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestOther.java
+++ HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestOther.java
@@ -58,20 +58,38 @@ public class WebTestOther extends BaseWebTest
   }
 
   @Test
-  public void testClientSideValidation()
+  public void testClientSideValidationExists()
   {
     startProcess("150425B095B4FB54/ClientSideValidationDemo.ivp");
     driver.findElement(By.id("form:firstName")).sendKeys("Someone");
     driver.findElement(By.id("form:personName")).sendKeys("Else");
     driver.findElement(By.id("form:personDateOfBirth")).sendKeys("1990-01-01");
     driver.findElement(By.id("form:addressZip")).sendKeys("8000");
-    driver.findElement(By.id("form:addressCity")).sendKeys("Zï¿½rich");
+    driver.findElement(By.id("form:addressCity")).sendKeys("Zürich");
     driver.findElement(By.id("form:addressCountry")).sendKeys("CH");
     driver.findElement(By.id("form:email")).sendKeys("someone.else@admin.ch");
     driver.findElement(By.id("form:timeOfArrival")).sendKeys("13:15");
     driver.findElement(By.id("form:proceed")).click();
     await(textToBePresentInElementLocated(By.id("welcomeText"),
             "Welcome to Axon.ivy Html Dialog Demos"));
+  }
+
+  @Test
+  public void testClientSideValidationWorks()
+  {
+    startProcess("150425B095B4FB54/ClientSideValidationDemo.ivp");
+    driver.findElement(By.id("form:firstName")).sendKeys("Someone");
+    driver.findElement(By.id("form:personName")).sendKeys("El");
+    driver.findElement(By.id("form:personDateOfBirth")).sendKeys("1990-01-01");
+    driver.findElement(By.id("form:addressZip")).sendKeys("999");
+    driver.findElement(By.id("form:addressCity")).sendKeys("Zürich");
+    driver.findElement(By.id("form:addressCountry")).sendKeys("CH");
+    driver.findElement(By.id("form:email")).sendKeys("someone.else@admin.ch");
+    driver.findElement(By.id("form:timeOfArrival")).sendKeys("13:15");
+    await(textToBePresentInElementLocated(By.xpath("//*[@data-target='form:personName']"),
+            "Length is less than allowable minimum of '3'"));
+    await(textToBePresentInElementLocated(By.xpath("//*[@data-target='form:addressZip']"),
+            "Specified attribute is not between the expected values of 1000 and 9999."));
   }
 
   private void openAndValidate(String managedBeanPoperty)
