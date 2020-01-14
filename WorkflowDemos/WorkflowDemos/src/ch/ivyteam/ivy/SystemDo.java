5025ada619a81ad2195ead2b47c654260380d414
Lukas Lieb <lukas.lieb@axonivy.com>
2020-01-14 13:34:36 +0100

    format WorkflowDemos

--- WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/SystemDo.java
+++ WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/SystemDo.java
@@ -4,32 +4,32 @@ import ch.ivyteam.ivy.security.exec.Sudo;
 import ch.ivyteam.ivy.workflow.ICase;
 import ch.ivyteam.ivy.workflow.ITask;
 
-public class SystemDo 
+public class SystemDo
 {
-	public static void setCustomVarCharField1(final ITask task, final String val)
-			throws Exception 
-	{
-		Sudo.exec(() -> task.customFields().stringField("CustomVarCharField1").set(val));
-	}
-	
-	public static void setCaseName(final ICase icase, final String val)
-			throws Exception 
-	{
-		Sudo.exec(() -> icase.setName(val));
-	}
-	
-	public static void setCaseDescription(final ICase icase, final String val)
-			throws Exception 
-	{
-		Sudo.exec(() -> icase.setDescription(val));
-	}	
-	
-	public static void setProcess(final ICase icase, final String code, final String val)
-			throws Exception 
-	{
-		Sudo.exec(() -> {
-			icase.customFields().stringField("ProcessCode").set(code);
-			icase.customFields().stringField("ProcessName").set(val);
-		});
-	}	
+  public static void setCustomVarCharField1(final ITask task, final String val)
+          throws Exception
+  {
+    Sudo.exec(() -> task.customFields().stringField("CustomVarCharField1").set(val));
+  }
+
+  public static void setCaseName(final ICase icase, final String val)
+          throws Exception
+  {
+    Sudo.exec(() -> icase.setName(val));
+  }
+
+  public static void setCaseDescription(final ICase icase, final String val)
+          throws Exception
+  {
+    Sudo.exec(() -> icase.setDescription(val));
+  }
+
+  public static void setProcess(final ICase icase, final String code, final String val)
+          throws Exception
+  {
+    Sudo.exec(() -> {
+      icase.customFields().stringField("ProcessCode").set(code);
+      icase.customFields().stringField("ProcessName").set(val);
+    });
+  }
 }
--- WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/wfdemo/businessdata/InterviewLazyDataModel.java
+++ WorkflowDemos/WorkflowDemos/src/ch/ivyteam/ivy/wfdemo/businessdata/InterviewLazyDataModel.java
@@ -1,30 +1,34 @@
 package ch.ivyteam.ivy.wfdemo.businessdata;
 
-import workflow.businesscasedata.Interview;
 import ch.ivyteam.ivy.business.data.store.search.Filter;
 import ch.ivyteam.ivy.business.data.store.search.Query;
+import workflow.businesscasedata.Interview;
 
-public class InterviewLazyDataModel extends AbstractBusinessDataLazyDataModel<Interview> {
+public class InterviewLazyDataModel extends AbstractBusinessDataLazyDataModel<Interview>
+{
 
-	private static final long serialVersionUID = -501730717404259085L;
-	private String wordsFilter = "";
+  private static final long serialVersionUID = -501730717404259085L;
+  private String wordsFilter = "";
 
-	public InterviewLazyDataModel() {
-		super(Interview.class);
-	}
-	
-	@Override
-	protected Filter<Interview> filter(Query<Interview> query)
-	{
-		return query.allFields().containsAllWordPatterns(wordsFilter);
-	}
+  public InterviewLazyDataModel()
+  {
+    super(Interview.class);
+  }
 
-	public String getWordsFilter() {
-		return wordsFilter;
-	}
+  @Override
+  protected Filter<Interview> filter(Query<Interview> query)
+  {
+    return query.allFields().containsAllWordPatterns(wordsFilter);
+  }
 
-	public void setWordsFilter(String wordsFilter) {
-		this.wordsFilter = wordsFilter;
-	}
+  public String getWordsFilter()
+  {
+    return wordsFilter;
+  }
+
+  public void setWordsFilter(String wordsFilter)
+  {
+    this.wordsFilter = wordsFilter;
+  }
 
 }
