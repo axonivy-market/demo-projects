00e4afdf8bd19bba96f2ec8ebc36510bc92c5752
Lukas Lieb <lukas.lieb@axonivy.com>
2020-01-30 11:52:09 +0100

    fix delete ivy class

--- QuickStartTutorial/src_dataClasses/quickStartTutorial/Data.java
+++ QuickStartTutorial/src_dataClasses/quickStartTutorial/Data.java
@@ -7,6 +7,66 @@ package quickStartTutorial;
 public class Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
 {
   /** SerialVersionUID */
-  private static final long serialVersionUID = 0L;
+  private static final long serialVersionUID = -3382065151791125891L;
+
+  private java.lang.String product;
+
+  /**
+   * Gets the field product.
+   * @return the value of the field product; may be null.
+   */
+  public java.lang.String getProduct()
+  {
+    return product;
+  }
+
+  /**
+   * Sets the field product.
+   * @param _product the new value of the field product.
+   */
+  public void setProduct(java.lang.String _product)
+  {
+    product = _product;
+  }
+
+  private ch.ivyteam.ivy.scripting.objects.Date releaseDate;
+
+  /**
+   * Gets the field releaseDate.
+   * @return the value of the field releaseDate; may be null.
+   */
+  public ch.ivyteam.ivy.scripting.objects.Date getReleaseDate()
+  {
+    return releaseDate;
+  }
+
+  /**
+   * Sets the field releaseDate.
+   * @param _releaseDate the new value of the field releaseDate.
+   */
+  public void setReleaseDate(ch.ivyteam.ivy.scripting.objects.Date _releaseDate)
+  {
+    releaseDate = _releaseDate;
+  }
+
+  private java.lang.Number price;
+
+  /**
+   * Gets the field price.
+   * @return the value of the field price; may be null.
+   */
+  public java.lang.Number getPrice()
+  {
+    return price;
+  }
+
+  /**
+   * Sets the field price.
+   * @param _price the new value of the field price.
+   */
+  public void setPrice(java.lang.Number _price)
+  {
+    price = _price;
+  }
 
 }
