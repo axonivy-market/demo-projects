eecd8b2e07a4d5a6bf92fa9ecbde8983337cee0c
Lukas Lieb <lukas.lieb@axonivy.com>
2020-03-16 13:51:35 +0100

    XIVY-3538 add test for static page in html dialog demos

---
+++ HtmlDialogDemos/HtmlDialogDemosTest/src_test/ch/ivyteam/htmldialog/demo/WebTestStaticPage.java
@@ -0,0 +1,22 @@
+package ch.ivyteam.htmldialog.demo;
+
+import static ch.ivyteam.htmldialog.server.test.ProcessUtil.startProcess;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.ivy.supplements.IvySelenide;
+
+@IvySelenide
+public class WebTestStaticPage {
+	
+  @Test
+  void testStaticPage()
+  {
+    startProcess("1698080EFEC9C585/StaticEndPage.ivp");
+    $$("button").find(text("Bean Magic")).click();
+    $("#mainArea").shouldHave(text("clicked the button"));
+  }
+}
