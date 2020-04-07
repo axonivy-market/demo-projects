51e6d5b822bd04e5f19f39b6a2c353ac4e37a7ae
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-04-07 08:54:04 +0200

    XIVY-3754 integration tests call sub with bpmClient call
    - verify that we are able to call a sub starting in a remote project.

---
+++ ConnectivityDemos/ConnectivityDemosTest/src_test/rest/bpm/TestApprovalCallSub.java
@@ -0,0 +1,27 @@
+package rest.bpm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.connectivity.test.CreateApprovalData;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+
+@IvyProcessTest
+public class TestApprovalCallSub 
+{
+  @Test
+  public void callSub(BpmClient bpmClient)
+  {
+    ExecutionResult result = bpmClient.start()
+      .process("rest/testApprovalCreation/test.ivp")
+      .as().user("restUser")
+      .execute();
+    CreateApprovalData data = result.getData().last();
+    assertThat(data.getCreatedTask().getName())
+      .isEqualTo("Approve request from restUser");
+  }
+}
