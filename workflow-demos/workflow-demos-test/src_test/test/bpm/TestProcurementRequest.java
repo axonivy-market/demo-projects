8553d3974c67bd36e978ccdc7353f9fb69e857e5
Lukas <42733123+ivy-lli@users.noreply.github.com>
2020-04-22 14:27:49 +0200

    Feature/xivy 3475 rename demos le (#59)
    rename demos
    rename groupids

---
+++ workflow-demos/workflow-demos-test/src_test/test/bpm/TestProcurementRequest.java
@@ -0,0 +1,224 @@
+package test.bpm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.security.exec.Sudo;
+import ch.ivyteam.ivy.workflow.CaseState;
+import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.ITask;
+import ch.ivyteam.ivy.workflow.TaskState;
+import workflow.humantask.ProcurementRequest;
+
+/**
+ * Early preview of {@link BpmClient} testing API usage. 
+ * 
+ * Here to expose difficulties when working with workflow centered processes.
+ * In order to improve until the release 9.1
+ *
+ * Tests the ProcurementRequest:
+ * <code>workflow-demos/processes/Humantask/ProcurementRequestParallel.mod</code>
+ * 
+ * @author rew
+ * @author rwei
+ * @since Mar 3, 2020
+ */
+@IvyProcessTest
+class TestProcurementRequest
+{
+  /** Processes under test */
+  private static final BpmProcess PROCUREMENT_PROCESS = BpmProcess.name("ProcurementRequestParallel");
+  
+  /** HTML Dialogs under test */
+  public static interface HtmlDialog
+  {
+    BpmElement ENTER_REQUEST = PROCUREMENT_PROCESS.elementName("Enter Request");
+    BpmElement VERIFY_REQUEST_TEAM_LEADER = PROCUREMENT_PROCESS.element().name().contains("by Team Leader");
+    BpmElement VERIFY_REQUEST_MANAGER = PROCUREMENT_PROCESS.element().name().contains("by Manager");
+    BpmElement ACCEPT_REQUEST = PROCUREMENT_PROCESS.elementName("Accept Request");
+  }  
+  
+  /**
+   * Manager and Team Leader do not verify
+   */
+  @Test
+  void approvalTask_notVerified_byBoth(BpmClient bpmClient)
+  {
+    List<ITask> verifyTasks = createProcurrementRequest(bpmClient);
+    
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_TEAM_LEADER).with(ProcurementRequest.class, (in, out) -> out.setDataOkTeamLeader(false));
+    verifyRequest(bpmClient, verifyTasks.get(0), "Teamleader");
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_MANAGER).with(ProcurementRequest.class, (in, out) -> out.setDataOkManager(false));
+    verifyRequest(bpmClient, verifyTasks.get(1), "Manager");
+    
+    ICase caze = verifyTasks.get(0).getCase();
+    waitUntilSystemTaskHasBeenExecuted(caze); 
+    
+    assertThat(caze.getState()).isEqualTo(CaseState.DONE);
+  }
+
+  /**
+   * Manager does not and Team Leader does verify
+   */
+  @Test
+  void approvalTask_notVerified_byManager(BpmClient bpmClient)
+  {
+    List<ITask> verifyTasks = createProcurrementRequest(bpmClient);
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_TEAM_LEADER).with(ProcurementRequest.class, (in, out) -> out.setDataOkTeamLeader(true));
+    verifyRequest(bpmClient, verifyTasks.get(0), "Teamleader");
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_MANAGER).with(ProcurementRequest.class, (in, out) -> out.setDataOkManager(false));
+    verifyRequest(bpmClient, verifyTasks.get(1), "Manager");
+    
+    ICase caze = verifyTasks.get(0).getCase();
+    waitUntilSystemTaskHasBeenExecuted(caze); 
+    
+    assertThat(caze.getState()).isEqualTo(CaseState.DONE);
+  }
+
+  /**
+   * Manager does and Team Leader does not verify
+   */
+  @Test
+  void approvalTask_notVerified_byTeamLeader(BpmClient bpmClient)
+  {
+    List<ITask> verifyTasks = createProcurrementRequest(bpmClient);
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_TEAM_LEADER).with(ProcurementRequest.class, (in, out) -> out.setDataOkTeamLeader(false));
+    verifyRequest(bpmClient, verifyTasks.get(0), "Teamleader");
+    
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_MANAGER).with(ProcurementRequest.class, (in, out) -> out.setDataOkManager(true));
+    verifyRequest(bpmClient, verifyTasks.get(1), "Manager");
+    
+    ICase caze = verifyTasks.get(0).getCase();
+    waitUntilSystemTaskHasBeenExecuted(caze); 
+    
+    assertThat(caze.getState()).isEqualTo(CaseState.DONE);
+  }
+
+ /**
+  * Manager and Team Leader do verify but Executive Manager does not accept 
+  */
+  @Test
+  void approvalTask_verified_notAccepted(BpmClient bpmClient)
+  {
+    List<ITask> verifyTasks = createProcurrementRequest(bpmClient);
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_TEAM_LEADER).with(ProcurementRequest.class, (in, out) -> out.setDataOkTeamLeader(true));
+    verifyRequest(bpmClient, verifyTasks.get(0), "Teamleader");
+
+    bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_MANAGER).with(ProcurementRequest.class, (in, out) -> out.setDataOkManager(true));
+    verifyRequest(bpmClient, verifyTasks.get(1), "Manager");
+    
+    ICase caze = verifyTasks.get(0).getCase();
+    waitUntilAcceptRequestTaskIsAvailable(caze);
+    
+    bpmClient.mock().element(HtmlDialog.ACCEPT_REQUEST).withNoAction();
+    ProcurementRequest request = acceptRequest(bpmClient, getAcceptRequestTask(caze));
+    
+    assertThat(request.getAccepted()).isFalse();
+    assertThat(caze.getState()).isEqualTo(CaseState.DONE);
+  }
+  
+  /**
+   * Manager and Team Leader do verify and Executive Manager does accept 
+   */
+   @Test
+   void approvalTask_verified_accepted(BpmClient bpmClient)
+   {
+     List<ITask> verifyTasks = createProcurrementRequest(bpmClient);
+
+     bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_TEAM_LEADER).with(ProcurementRequest.class, (in, out) -> out.setDataOkTeamLeader(true));
+     verifyRequest(bpmClient, verifyTasks.get(0), "Teamleader");
+
+     bpmClient.mock().element(HtmlDialog.VERIFY_REQUEST_MANAGER).with(ProcurementRequest.class, (in, out) -> out.setDataOkManager(true));
+     verifyRequest(bpmClient, verifyTasks.get(1), "Manager");
+
+     ICase caze = verifyTasks.get(0).getCase();
+     waitUntilAcceptRequestTaskIsAvailable(caze);
+     
+     bpmClient.mock().element(HtmlDialog.ACCEPT_REQUEST).with(ProcurementRequest.class, (in, out) -> out.setAccepted(true));
+     ProcurementRequest request = acceptRequest(bpmClient, getAcceptRequestTask(caze));
+     
+     assertThat(request.getAccepted()).isTrue();
+     assertThat(caze.getState()).isEqualTo(CaseState.DONE);
+   }
+
+  private List<ITask> createProcurrementRequest(BpmClient bpmClient)
+  {
+    var testData = new ProcurementRequest();
+    testData.setDescription("PC");
+    testData.setAmount(3);
+    testData.setPricePerUnit(1222.95);
+    testData.setTotalPrice(3668.85);
+
+    bpmClient.mock().element(HtmlDialog.ENTER_REQUEST).with(in -> testData);
+
+    ExecutionResult result = bpmClient
+        .start().process(PROCUREMENT_PROCESS)
+        .as().user("ldv")
+        .execute();
+    assertThat(result).isNotNull();
+    List<ITask> tasks = result.getRequestedCase().getActiveTasks();
+    assertThat(tasks).hasSize(2);
+    ITask verifyTask1 = tasks.get(0);
+    assertThat(verifyTask1.getName()).startsWith("Verify Request:");
+    assertThat(verifyTask1.getActivatorName()).isEqualTo("Teamleader");
+    ITask verifyTask2 = tasks.get(1);
+    assertThat(verifyTask2.getName()).startsWith("Verify Request:");
+    assertThat(verifyTask2.getActivatorName()).isEqualTo("Manager");
+    return tasks;
+  }
+
+  private void verifyRequest(BpmClient bpmClient, ITask verifyTask, String role)
+  {     
+    ExecutionResult result = bpmClient
+        .start().resumableTask(verifyTask)
+        .as().role(role)        
+        .execute();
+    assertThat(result.getRequestedTask().getState()).isIn(TaskState.DONE, TaskState.READY_FOR_JOIN);
+  }
+  
+  private ProcurementRequest acceptRequest(BpmClient bpmClient, ITask acceptRequestTask)
+  {
+    ExecutionResult result = bpmClient.start().resumableTask(acceptRequestTask).as().role("Executive Manager").execute();
+    assertThat(result.getRequestedTask().getState()).isIn(TaskState.DONE);
+    return result.getData().last();
+  }
+  
+  //TODO API Sugar: set locale to default cms language. Provide api to set other language
+  private ITask getAcceptRequestTask(ICase caze)
+  {
+    // Locale of system user session seems to be the default locale of the os
+    String taskName = Locale.ENGLISH.getLanguage().equals(Locale.getDefault().getLanguage()) ? "Accept Request:" : "Antrag annehmen:";
+    return caze
+        .getActiveTasks()
+        .stream()
+        .filter(task -> task.getName().contains(taskName))  
+        .findAny()
+        .orElse(null);
+  }
+  
+  // TODO API Sugar: Provide method to wait until system task has been executed 
+  private void waitUntilSystemTaskHasBeenExecuted(ICase caze)
+  {
+    Awaitility.await().until(()-> Sudo.exec(() -> caze.getState() == CaseState.DONE));
+  }
+
+  private void waitUntilAcceptRequestTaskIsAvailable(ICase caze)
+  {
+    Awaitility.await().until(()-> Sudo.exec(() -> getAcceptRequestTask(caze) != null));
+  }
+}
---
+++ workflow-demos/workflow-demos-test/src_test/workflow/trigger/WebTestCreateNewEmployee.java
@@ -0,0 +1,35 @@
+package workflow.trigger;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import com.axonivy.ivy.webtest.IvyWebTest;
+import com.axonivy.ivy.webtest.engine.EngineUrl;
+import com.codeborne.selenide.Selenide;
+
+@IvyWebTest
+public class WebTestCreateNewEmployee
+{
+
+  @Test
+  public void testCreateNewEmployee()
+  {
+    open(EngineUrl.base());
+    $(By.linkText("InitializeTestEnvironment/start.ivp")).shouldBe(visible);
+    assertThat(Selenide.title().contains("ivy"));
+    $(By.linkText("InitializeTestEnvironment/start.ivp")).click();
+    open(EngineUrl.createProcessUrl("/workflow-demos/15254CF1CE56AE72/start.ivp"));
+    $("#form\\:employeeName").sendKeys("Charlie");
+    $("#form\\:email").sendKeys("charlie@xyz.com");
+    $("#form\\:employeeDateOfBirth_input").sendKeys("26.12.1985");
+    $("#ui-datepicker-div").shouldBe(visible);
+    $("#form\\:employeeName").click();
+    $("#ui-datepicker-div").shouldNotBe(visible);
+    $("#form\\:needsParkingLot").click();
+  }
+}
