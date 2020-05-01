aed1db67a79d6ae8660b5d4c170bb76ce0e6167d
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-05-01 14:26:39 +0200

    tenent as restClient property

--- connectivity/connectivity-demos/src/com/axonivy/connectivity/rest/sample/uipath/TenantHeaderFeature.java
+++ connectivity/connectivity-demos/src/com/axonivy/connectivity/rest/sample/uipath/TenantHeaderFeature.java
@@ -1,13 +1,15 @@
 package com.axonivy.connectivity.rest.sample.uipath;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.ws.rs.Priorities;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class TenantHeaderFeature implements Feature
 {
@@ -16,10 +18,24 @@ public class TenantHeaderFeature implements Feature
   @Override
   public boolean configure(FeatureContext context)
   {
-    Configuration config = context.getConfiguration();
-    String tenant = (String) config.getProperties().getOrDefault("tenant", "AXONPRESALES");
+    Map<String, Object> properties = context.getConfiguration().getProperties();
+    String tenant = readMandatory(properties, "tenant");
     context.register(new OrechstratorTenantFilter(tenant), Priorities.HEADER_DECORATOR);
     return true;
+  }
+
+  private static String readMandatory(Map<String, Object> properties, String key)
+  {
+    Object value = properties.get(key);
+    if (value instanceof String)
+    {
+      String tenant = (String) value;
+      if (StringUtils.isNotEmpty(tenant))
+      {
+        return tenant;
+      }
+    }
+    throw new IllegalStateException("Missing property '"+key+"' on for "+TenantHeaderFeature.class.getName()+".");
   }
 
   private static class OrechstratorTenantFilter implements ClientRequestFilter
