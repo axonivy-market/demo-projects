1cd06006393f2c92043453c2b080ed9f96ec7410
Lukas <42733123+ivy-lli@users.noreply.github.com>
2020-05-20 09:29:32 +0200

    use current instead of depricated get (#100)
    * use current instead of depricated get
    
    * fix all deprecations

--- workflow/workflow-demos/src/ch/ivyteam/ivy/wfdemo/businessdata/DemoDataCreator.java
+++ workflow/workflow-demos/src/ch/ivyteam/ivy/wfdemo/businessdata/DemoDataCreator.java
@@ -12,7 +12,7 @@ public class DemoDataCreator
 
   public static void createDemoDataIfNotExist()
   {
-    BusinessDataRepository repo = BusinessDataRepository.get();
+    BusinessDataRepository repo = BusinessDataRepository.current();
     long dossierCount = countDossier(repo);
 
     if (dossierCount == 0)
@@ -84,13 +84,13 @@ public class DemoDataCreator
     person.setAddress(address);
     dossier.setPerson(person);
 
-    BusinessDataRepository repo = BusinessDataRepository.get();
+    BusinessDataRepository repo = BusinessDataRepository.current();
     repo.save(dossier);
   }
 
   public static void clearDemoData()
   {
-    BusinessDataRepository repo = BusinessDataRepository.get();
+    BusinessDataRepository repo = BusinessDataRepository.current();
     for (Dossier info : getDossiers(repo).getAll())
     {
       repo.delete(info);
