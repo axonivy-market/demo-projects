e46ef6e25661f5b904183a7ebd32ee2ff6b3d783
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-09-21 17:01:18 +0200

    read Gender into a custom wrapper clazz

--- connectivity/connectivity-demos/src/com/axonivy/connectivity/rest/sample/odata/TripPinJsonFeature.java
+++ connectivity/connectivity-demos/src/com/axonivy/connectivity/rest/sample/odata/TripPinJsonFeature.java
@@ -1,5 +1,7 @@
 package com.axonivy.connectivity.rest.sample.odata;
 
+import java.io.IOException;
+
 import javax.ws.rs.Priorities;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.MediaType;
@@ -8,13 +10,19 @@ import org.odata.trippin.client.AnyOfMicrosoftODataSampleServiceModelsTripPinPer
 import org.odata.trippin.client.AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender;
 
 import com.axonivy.connectivity.rest.json.OpenApiJsonFeature;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 /**
  * Some values in the converted TripPin ODATA spec are generated as empty interfaces without a matching impl. 
- * So let's mark them as skippable entities.
+ * 
+ * So let's handle them to make object transformation possible.
+ * 
  * @since 9.2
  */
 public class TripPinJsonFeature extends OpenApiJsonFeature
@@ -34,23 +42,57 @@ public class TripPinJsonFeature extends OpenApiJsonFeature
     public ObjectMapper locateMapper(Class<?> type, MediaType mediaType)
     {
       var mapper = super.locateMapper(type, mediaType);
-      mapper.registerModule(new TypeCustomizations());
+      mapper.registerModule(new TripPinTypeCustomizations());
       return mapper;
     }
   }
   
-  private static class TypeCustomizations extends SimpleModule
+  private static class TripPinTypeCustomizations extends SimpleModule
   {
-    public TypeCustomizations()
+    public TripPinTypeCustomizations()
     {
-      nullify(AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender.class);
-      nullify(AnyOfMicrosoftODataSampleServiceModelsTripPinPersonConcurrency.class);
+      nullify(AnyOfMicrosoftODataSampleServiceModelsTripPinPersonConcurrency.class); // ignore 'Concurrency' field.
+      addDeserializer(AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender.class, new GenderDeserializer());
     }
     
     @SuppressWarnings({"unchecked", "rawtypes"})
     private void nullify(Class<?> raw)
     {
       addDeserializer(raw, new Nullifier(raw));
+    }
+  }
+  
+  private static class GenderDeserializer extends StdDeserializer<AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender>
+  {
+    public GenderDeserializer()
+    {
+      super(AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender.class);
+    }
+
+    @Override
+    public AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender deserialize(JsonParser p, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException
+    {
+      return new GenderWrapper(p.getText());
+    }
+  }
+  
+  /**
+   * simple custom created string wrapper: since generated JAX-RS client interface has no valid impl.
+   */
+  public static class GenderWrapper implements AnyOfMicrosoftODataSampleServiceModelsTripPinPersonGender
+  {
+    private final String gender;
+    
+    public GenderWrapper(String gender)
+    {
+      this.gender = gender;
+    }
+    
+    @Override
+    public String toString()
+    {
+      return gender;
     }
   }
 }
