b88587a56321fe497a44eef103918a9af64d67a6
Alexander Suter <alexander.suter@axonivy.com>
2020-09-28 09:57:40 +0200

    wip

--- rule-engine/rule-engine-demos/src_test/ch/ivyteam/rule/engine/demo/RuleTestDrl.java
+++ rule-engine/rule-engine-demos/src_test/ch/ivyteam/rule/engine/demo/RuleTestDrl.java
@@ -1,13 +1,12 @@
 package ch.ivyteam.rule.engine.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import ch.ivyteam.ivy.environment.IvyTest;
@@ -24,7 +23,7 @@ class RuleTestDrl
     var testCases = Files
             .lines(Path.of("src_test/ch/ivyteam/rule/engine/demo/test-cases.csv"), StandardCharsets.UTF_8)
             .skip(1)
-            .map(this::toTestCase)
+            .map(RuleTestDrl::toTestCase)
             .collect(Collectors.toList());
 
     for (var testCase : testCases)
@@ -38,11 +37,11 @@ class RuleTestDrl
               .executor()
               .execute(member);
 
-      assertThat(member.getDiscount()).isEqualTo(testCase.discount);
+      Assertions.assertEquals(testCase.discount, member.getDiscount());
     }
   }
 
-  private TestCase toTestCase(String line)
+  private static TestCase toTestCase(String line)
   {
     var data = line.split(",");
 
