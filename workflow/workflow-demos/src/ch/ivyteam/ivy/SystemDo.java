e031adff3094bcab326d797d0a5ea10dab46bddf
Reto Weiss <reto.weiss@axonivy.com>
2021-11-10 15:29:01 +0100

    XIVY-6926 suppress deprecation warning Sudo.exec
    I cannot fix Sudo.exec to Sudo.run because then the demo will no longer
    work on ivy < 8.0.20
    
    I therefore simply suppress the deprecation warning here

--- workflow/workflow-demos/src/ch/ivyteam/ivy/SystemDo.java
+++ workflow/workflow-demos/src/ch/ivyteam/ivy/SystemDo.java
@@ -6,24 +6,28 @@ import ch.ivyteam.ivy.workflow.ITask;
 
 public class SystemDo
 {
+  @SuppressWarnings("deprecation")
   public static void setCustomVarCharField1(final ITask task, final String val)
           throws Exception
   {
     Sudo.exec(() -> task.customFields().stringField("CustomVarCharField1").set(val));
   }
 
+  @SuppressWarnings("deprecation")
   public static void setCaseName(final ICase icase, final String val)
           throws Exception
   {
     Sudo.exec(() -> icase.setName(val));
   }
 
+  @SuppressWarnings("deprecation")
   public static void setCaseDescription(final ICase icase, final String val)
           throws Exception
   {
     Sudo.exec(() -> icase.setDescription(val));
   }
 
+  @SuppressWarnings("deprecation")
   public static void setProcess(final ICase icase, final String code, final String val)
           throws Exception
   {
