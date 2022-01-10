8e13a493fcc190cf24a74babef9e45b5f6780d0a
Lukas Lieb <lukas.lieb@axonivy.com>
2022-01-10 13:57:52 +0100

    Improve/fix rule web test

--- rule-engine/rule-engine-demos-test/src_test/ch/ivyteam/rule/engine/demo/WebTestAdjustableDiscountCalculatorIT.java
+++ rule-engine/rule-engine-demos-test/src_test/ch/ivyteam/rule/engine/demo/WebTestAdjustableDiscountCalculatorIT.java
@@ -26,10 +26,8 @@ public class WebTestAdjustableDiscountCalculatorIT
   private static final String MEMBER_TYPE = "form:memberType";
   private static final String APPLY_DRL = "form:applyDRL";
   private static final String RULETABLE_SAVE_BUTTON = "tableform:save";
-  private static final String RULETABLE_ROW2_EDIT = "tableform:j_id_l:2:j_id_16";
-  private static final String RULETABLE_ROW2_DISCOUNT_EDIT = "tableform:j_id_l:2:j_id_14";
   private static final String RULETABLE_SAVED_MSG = "tableform:saved_msg";
-  
+
   @BeforeEach
   public void openRuleDemo()
   {
@@ -50,13 +48,16 @@ public class WebTestAdjustableDiscountCalculatorIT
     setMemberType("Silver");
     assertDiscountForAmount(900, 5);
 
-    $(By.id(RULETABLE_ROW2_EDIT)).shouldBe(visible).click();
-    $(By.id(RULETABLE_ROW2_DISCOUNT_EDIT)).sendKeys(Keys.BACK_SPACE);
-    $(By.id(RULETABLE_ROW2_DISCOUNT_EDIT)).sendKeys("7");
-    $(By.id(RULETABLE_ROW2_EDIT)).shouldBe(visible).click();    
+    var tableRow2 = $(By.id("tableform:table_data")).find(".ui-widget-content", 2);
+
+    tableRow2.find(".ui-row-editor").shouldBe(visible).click();
+    var discountInput = tableRow2.find(".ui-editable-column", 3).find(".ui-inputfield").shouldBe(visible);
+    discountInput.sendKeys(Keys.BACK_SPACE);
+    discountInput.sendKeys("7");
+    tableRow2.find(".ui-row-editor-check").shouldBe(visible).click();
     $(By.id(RULETABLE_SAVE_BUTTON)).shouldBe(visible).click();
     $(By.id(RULETABLE_SAVED_MSG)).shouldBe(visible);
-       
+
     assertDiscountForAmount(900, 7);
   }
 
@@ -72,7 +73,7 @@ public class WebTestAdjustableDiscountCalculatorIT
     $(By.id(APPLY_DRL)).shouldBe(visible).click();
     $(By.id(MAIN_MESSAGE)).shouldBe(text("Discount NOT defined"));
   }
-  
+
   private void assertDiscountForAmount(int amount, int discount)
   {
     setPurchaseAmount(amount);
