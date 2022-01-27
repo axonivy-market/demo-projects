c0599c89eb90e071296f547209dfaf377326c72d
Alexander Suter <alexander.suter@axonivy.com>
2022-01-27 14:46:48 +0100

    Fix disabled policy test

--- connectivity/connectivity-demos-test/src_test/com/axonivy/connectivity/soap/IntegrationTestSoapIgnorePolicy.java
+++ connectivity/connectivity-demos-test/src_test/com/axonivy/connectivity/soap/IntegrationTestSoapIgnorePolicy.java
@@ -2,40 +2,25 @@ package com.axonivy.connectivity.soap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 
-public class IntegrationTestSoapIgnorePolicy
+class IntegrationTestSoapIgnorePolicy
 {
   private static final String PROCESS_ID = "162AFFB8ACCCB74C";
 
   @Test
-  public void greetWithPolicyEnabled() throws Exception
+  void greetWithPolicyDisabled() throws Exception
   {
-    String url = EngineUrl.createProcessUrl("/connectivity-demos-test/" + PROCESS_ID
-            + "/greeterPoliciesNotDisabled.ivp");
-    CloseableHttpClient client = HttpClients.createDefault();
-    CloseableHttpResponse response = client.execute(new HttpGet(url));
-    String content = EntityUtils.toString(response.getEntity());
-    assertThat(content).isEqualTo(
-            "org.apache.cxf.ws.policy.PolicyException: None of the policy alternatives can be satisfied.");
-  }
-
-  @Test
-  public void greetWithPolicyDisabled() throws Exception
-  {
-    String url = EngineUrl.createProcessUrl("/connectivity-demos-test/" + PROCESS_ID
-            + "/greeterPoliciesDisabled.ivp");
-    CloseableHttpClient client = HttpClients.createDefault();
-    CloseableHttpResponse response = client.execute(new HttpGet(url));
-    String content = EntityUtils.toString(response.getEntity());
-    assertThat(content).isEqualTo(
-            "org.apache.cxf.binding.soap.SoapFault: A security error was encountered when verifying the message");
+    var url = EngineUrl.createProcessUrl("/connectivity-demos-test/" + PROCESS_ID + "/greeterPoliciesDisabled.ivp");
+    var httpRequest = HttpRequest.newBuilder(URI.create(url)).build();
+    var content =  HttpClient.newHttpClient().send(httpRequest, BodyHandlers.ofString()).body();
+    assertThat(content).isEqualTo("org.apache.cxf.binding.soap.SoapFault: A security error was encountered when verifying the message");
   }
 }
