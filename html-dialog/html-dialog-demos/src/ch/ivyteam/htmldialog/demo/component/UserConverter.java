c4f54ea0e4a8847344c9ad5ee6827423fcbef0f1
Alexander Suter <alexander.suter@axonivy.com>
2022-02-16 22:11:29 +0100

    Fix deprecation warning

--- html-dialog/html-dialog-demos/src/ch/ivyteam/htmldialog/demo/component/UserConverter.java
+++ html-dialog/html-dialog-demos/src/ch/ivyteam/htmldialog/demo/component/UserConverter.java
@@ -10,26 +10,18 @@ import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.IUser;
 
 @FacesConverter(value = "userConverter")
-public class UserConverter implements Converter
-{
+public class UserConverter implements Converter {
 
   @Override
-  public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2)
-          throws ConverterException
-  {
-    return Ivy.security().users().find(Long.parseLong(arg2));
+  public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) throws ConverterException {
+    return Ivy.security().users().findById(arg2);
   }
 
   @Override
-  public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2)
-          throws ConverterException
-  {
-
-    if (arg2 instanceof IUser)
-    {
-      return String.valueOf(((IUser) arg2).getId());
+  public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) throws ConverterException {
+    if (arg2 instanceof IUser) {
+      return ((IUser) arg2).getSecurityMemberId();
     }
     return null;
   }
-
 }
