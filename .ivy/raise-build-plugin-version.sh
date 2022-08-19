0b4d2621873a312b196f581d003a846f62a0e3b4
Alexander Suter <alexander.suter@axonivy.com>
2022-08-19 08:35:47 +0200

    Rename project.build.plugin.version property so that dependabot works

--- .ivy/raise-build-plugin-version.sh
+++ .ivy/raise-build-plugin-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn --batch-mode versions:set-property versions:commit -Dproperty=project.build.plugin.version -DnewVersion=${2} -DprocessAllModules -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -Dproperty=project-build-plugin -DnewVersion=${2} -DprocessAllModules -DallowSnapshots=true
