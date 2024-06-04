f89134080d168bbd180c2af7ed978325235b6238
Lukas Giger <lukas.giger@axonivy.com>
2024-06-04 12:43:20 +0200

    XIVY-13669 remove usage of NotificationRepository

--- workflow/workflow-demos-test/src_test/test/bpm/TestOrder.java
+++ workflow/workflow-demos-test/src_test/test/bpm/TestOrder.java
@@ -8,9 +8,7 @@ import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.notification.impl.NotificationRepository;
-import ch.ivyteam.ivy.notification.query.NotificationQuery;
+import ch.ivyteam.ivy.environment.NotificationAccess;
 import ch.ivyteam.ivy.scripting.objects.Tuple;
 import ch.ivyteam.ivy.workflow.CaseState;
 import workflow.order.Order;
@@ -20,7 +18,7 @@ import workflow.order.Person;
 public class TestOrder
 {
   private static final BpmProcess ORDER_PROCESS = BpmProcess.name("OrderProcess");
-  
+
   private static final BpmElement START_ORDER = ORDER_PROCESS.elementName("startOrder");
   private static final BpmElement INPUT_ORDER = ORDER_PROCESS.elementName("Input Order");
   private static final BpmElement PROCESS_ORDER = ORDER_PROCESS.elementName("Process Order");
@@ -31,15 +29,15 @@ public class TestOrder
   private static final Person CUSTOMER = new Person();
   private static final Order ORDER = new Order();
 
-  static 
+  static
   {
     CUSTOMER.setName("Doe");
     CUSTOMER.setSurname("John");
     ORDER.setProduct("Ferrari Testarossa");
   }
-  
+
   @Test
-  void runProcess(BpmClient bpmClient)
+  void runProcess(BpmClient bpmClient, NotificationAccess notifications)
   {
     bpmClient.mock().uiOf(INPUT_ORDER).with(this::inputOrder);
     bpmClient.mock().uiOf(PROCESS_ORDER).withNoAction();
@@ -48,8 +46,7 @@ public class TestOrder
     bpmClient.mock().uiOf(SEND_INVOICE).withNoAction();
     bpmClient.mock().uiOf(BOOK_PAYMENT).withNoAction();
 
-    NotificationQuery nofiticationQuery = NotificationRepository.of(Ivy.wf().getSecurityContext()).query();
-    var notifications = nofiticationQuery.executor().count();
+    var notificationsCount = notifications.count();
 
     var result = bpmClient.start().process(START_ORDER)
             .as().everybody().execute();
@@ -57,19 +54,19 @@ public class TestOrder
     bpmClient.start().anyActiveTask(result).as().role("Deliverer").execute();
     bpmClient.start().anyActiveTask(result).as().role("Finance").execute();
     bpmClient.start().anyActiveTask(result).as().role("Finance").execute();
-    
+
     assertThat(result.workflow().activeCase().getState()).isEqualTo(CaseState.DONE);
-    assertThat(nofiticationQuery.executor().count()).isEqualTo(notifications + 9);
+    assertThat(notifications.count()).isEqualTo(notificationsCount + 9);
   }
 
-  private void inputOrder(Tuple parameters, Tuple results) 
+  private void inputOrder(Tuple parameters, Tuple results)
   {
-    try 
+    try
     {
       results.set("customer", CUSTOMER);
       results.set("order", ORDER);
-    } 
-    catch (NoSuchFieldException ex) 
+    }
+    catch (NoSuchFieldException ex)
     {
       throw new RuntimeException(ex);
     }
