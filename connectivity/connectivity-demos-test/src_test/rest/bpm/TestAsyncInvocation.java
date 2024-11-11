547577d352c455976c2e1690fa9e6dad069c6624
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-11-11 09:42:37 +0100

    harden flacky test: assert graceful up to 10 seconds for the async available result

--- connectivity/connectivity-demos-test/src_test/rest/bpm/TestAsyncInvocation.java
+++ connectivity/connectivity-demos-test/src_test/rest/bpm/TestAsyncInvocation.java
@@ -3,14 +3,22 @@ package rest.bpm;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.process.model.value.SignalCode;
 import ch.ivyteam.ivy.workflow.query.SignalEventQuery;
+import ch.ivyteam.ivy.workflow.signal.ISignalEvent;
 
 @IvyProcessTest(enableWebServer = true)
 public class TestAsyncInvocation
@@ -22,12 +30,17 @@ public class TestAsyncInvocation
     ExecutionResult result = bpmClient.start().process("rest/asyncInvocation/callBatchAsync.ivp").execute();
     assertThat(result).isNotNull();
     
-    Thread.sleep(4000);
-    SignalEventQuery query = Ivy.wf().signals().history()
-            .createSignalEventQuery();
-    var fired = new ArrayList<>(query.executor().results());
-    assertThat(fired).isNotEmpty();
-    assertThat(fired.get(0).getSignalCode().toString()).isEqualTo("rest:batch:result:ok");
+    SignalEventQuery query = Ivy.wf().signals().history().createSignalEventQuery();
+    
+    Supplier<List<String>> signals = () -> new ArrayList<>(query.executor().results())
+      .stream().map(ISignalEvent::getSignalCode).map(SignalCode::toString)
+      .toList();
+    Awaitility.await()
+      .atLeast(3, TimeUnit.SECONDS) // default delay: on process being called
+      .atMost(10, TimeUnit.SECONDS)
+      .until(() -> signals.get(), 
+        codes -> !codes.isEmpty() && Objects.equals(codes.get(0), "rest:batch:result:ok")
+      );
   }
   
 }
